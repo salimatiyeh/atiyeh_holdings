051e45d47660bf6a39497ee80ba236f30c714561
salimatiyeh <131699477+salimatiyeh@users.noreply.github.com>
2023-10-17 11:24:21 -0500

    mapbox works but address autocomplete does not

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,11 +1,19 @@
 // Import and register all your controllers from the importmap under controllers/*
 
 import { application } from "controllers/application"
-
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
+
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
+import address_autocomplete_controller from "./address_autocomplete_controller"
+application.register("address-autocomplete", address_autocomplete_controller)
+
+import map_controller from "./map_controller"
+application.register("map", map_controller)
+
+import hello_controller from "./hello_controller"
+application.register("hello", hello_controller)
--- app/models/house.rb
+++ app/models/house.rb
@@ -1,3 +1,6 @@
 class House < ApplicationRecord
+  geocoded_by :address
   has_one_attached :photo
+
+  after_validation :geocode, if: :will_save_change_to_address?
 end
