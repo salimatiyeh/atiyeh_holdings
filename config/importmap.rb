469dcda2e38fa536ec0eb8583c247ec1ea56b6aa
salimatiyeh <131699477+salimatiyeh@users.noreply.github.com>
2023-10-18 14:36:55 -0500

    refreshed cloudinary key to see if it works in heroku

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,6 +6,7 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
-pin "@popperjs/core", to: "popper.js", preload: true
+# pin "@popperjs/core", to: "popper.js", preload: true
+pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.2/dist/esm/index.js"
 pin "mapbox-gl", to: "https://ga.jspm.io/npm:mapbox-gl@2.15.0/dist/mapbox-gl.js"
 pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.1/nodelibs/browser/process-production.js"
