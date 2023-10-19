a3bacdfd0b70e6c3b878cd172940e9eaeb578aae
salimatiyeh <131699477+salimatiyeh@users.noreply.github.com>
2023-10-19 14:35:25 -0500

    added and removed some random stuff

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,10 +1,20 @@
 // Import and register all your controllers from the importmap under controllers/*
 
-import { application } from "controllers/application"
-// Eager load all controllers defined in the import map under controllers/**/*_controller
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+// import { application } from "controllers/application"
+// // Eager load all controllers defined in the import map under controllers/**/*_controller
+// import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+// eagerLoadControllersFrom("controllers", application)
 
+import { application } from "./application"
+
+import AddressAutocompleteController from "./address_autocomplete_controller"
+application.register("address-autocomplete", AddressAutocompleteController)
+
+import HelloController from "./hello_controller"
+application.register("hello", HelloController)
+
+import MapController from "./map_controller"
+application.register("map", MapController)
 
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
