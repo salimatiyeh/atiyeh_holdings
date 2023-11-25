9f4749216ca1613eb276b37421ace54a9052c172
Jasim Atiyeh <jasimatiyeh@gmail.com>
2023-11-25 17:08:19 -0600

    Add routes for frontend react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Houses from "./components/Houses.jsx";
-import HouseDetails from "./components/HouseDetails.jsx";
+import House from "./components/House.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/houses/:id" element={<HouseDetails />} />
-        <Route path="/houses" element={<Houses />} />
-      </Routes>
-    </Router>
+    <>
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/houses/:id" element={<House />} />
+          <Route path="/houses" element={<Houses />} />
+        </Routes>
+      </Router>
+    </>
   );
 };
 
